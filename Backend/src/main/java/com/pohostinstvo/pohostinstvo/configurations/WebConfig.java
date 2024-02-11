777b28e2ca4c99b4be3b2e326a2a27b38cff466a
miriamGmitterova <gmitterova.miriam@gmail.com>
2024-02-11 19:14:12 +0100

    Upload web configu

---
+++ Backend/src/main/java/com/pohostinstvo/pohostinstvo/configurations/WebConfig.java
@@ -0,0 +1,20 @@
+package com.pohostinstvo.pohostinstvo.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+//zobrane z tutorialu
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*").allowedOrigins("*");
+    }
+}
