b161f80a8841f2d05420a1f1558d4f93ab295c6e
miriamGmitterova <gmitterova.miriam@gmail.com>
2024-02-11 19:12:26 +0100

    BE - zaklad projektu

---
+++ Backend/src/test/java/com/pohostinstvo/pohostinstvo/PohostinstvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pohostinstvo.pohostinstvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PohostinstvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
